9b6e8e8382aeea11d56e4cde984881a0b0dbcfc5
Freshta-Abbasi <freshtaabbasi100@gmail.com>
2026-03-04 22:18:01 +0430

    Week08-Day02-Assignment01

--- Week08/Day02/Assignment01/index.js
+++ Week08/Day02/Assignment01/index.js
@@ -1,4 +1,12 @@
-function bmiCalc(w,h){
-    bmi=w/(h*h);
-    return bmi;
+function bmiCalculator(w, h) {
+    var bmi = w/(h*h);
+    
+    if (bmi < 18.5) {
+        return "Your BMI is " + bmi + ", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        return "Your BMI is " + bmi + ", so you have a normal weight.";
+    } else {
+        return "Your BMI is " + bmi + ", so you are overweight.";
+    }
 }
+bmiCalculator(40, 1.5);
